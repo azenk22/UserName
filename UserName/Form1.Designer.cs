126b86d1cabbc1d5e8c56cfd5bdfba295e154739
Alex Zenk <azenk@css.edu>
2021-04-25 16:56:13 -0500

    Initial

--- UserName/Form1.Designer.cs
+++ UserName/Form1.Designer.cs
@@ -28,13 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UsernameTB = new System.Windows.Forms.TextBox();
+            this.ValidateBTN = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // UsernameTB
+            // 
+            this.UsernameTB.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.UsernameTB.Location = new System.Drawing.Point(73, 7);
+            this.UsernameTB.Name = "UsernameTB";
+            this.UsernameTB.Size = new System.Drawing.Size(115, 20);
+            this.UsernameTB.TabIndex = 1;
+            // 
+            // ValidateBTN
+            // 
+            this.ValidateBTN.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.ValidateBTN.Location = new System.Drawing.Point(73, 37);
+            this.ValidateBTN.Name = "ValidateBTN";
+            this.ValidateBTN.Size = new System.Drawing.Size(76, 28);
+            this.ValidateBTN.TabIndex = 2;
+            this.ValidateBTN.Text = "Validate";
+            this.ValidateBTN.UseVisualStyleBackColor = true;
+            this.ValidateBTN.Click += new System.EventHandler(this.ValidateBTN_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.91667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 72.08334F));
+            this.tableLayoutPanel1.Controls.Add(this.ValidateBTN, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.UsernameTB, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45.67901F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 54.32099F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(240, 71);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(276, 103);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UsernameTB;
+        private System.Windows.Forms.Button ValidateBTN;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
--- UserName/Form1.cs
+++ UserName/Form1.cs
@@ -16,5 +16,102 @@ namespace UserName
         {
             InitializeComponent();
         }
+
+        private void ValidateBTN_Click(object sender, EventArgs e)
+        {
+
+            if(ValidUsername())
+            {
+                
+                MessageBox.Show("That username is valid!", "Valid Username");
+
+
+            }
+            else
+            {
+                MessageBox.Show("That username is not valid!", "Invalid Username");
+            }
+
+        }
+
+        private bool ValidUsername()
+        {
+            bool valid = true;
+
+
+            if(!StartsWithLetter())
+            {
+                valid = false;
+            }
+
+            if (!Length())
+            {
+                valid = false;
+            }
+
+            if(!CheckChars())
+            {
+                valid = false;
+            }
+           
+            if(!LastChar())
+            {
+                valid = false;  
+            }
+            
+            return valid;
+        }
+
+        private bool StartsWithLetter()
+        {
+            bool letter = false;
+           
+            letter = Char.IsLetter(UsernameTB.Text[0]);
+
+            return letter;
+        }
+
+        private bool Length()
+        {
+            bool length = false;
+
+            if(UsernameTB.Text.Length >= 4 && UsernameTB.Text.Length <= 25)
+            {
+                length = true;
+            }
+
+            return length;
+        }
+
+        private bool CheckChars()
+        {
+            bool chars = true;
+
+            foreach(char c in UsernameTB.Text)
+            {
+
+                if(!Char.IsLetter(c) && !Char.IsNumber(c) && c!= '_')
+                {
+                    chars = false;
+                }
+
+            }
+
+            return chars;
+        }
+
+        private bool LastChar()
+        {
+            bool last = false;
+
+            char lastChar = UsernameTB.Text.Last();
+
+            if(lastChar != '_')
+            {
+                last = true;
+            }
+
+            return last;
+        }
     }
 }
